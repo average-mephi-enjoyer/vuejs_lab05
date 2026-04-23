90cff2ce2f643793fec7065a3b8814154e73645a
Yashchenko Artem <artemyaschenko@inbox.ru>
2026-04-23 21:36:46 +0300

    vue installed in task3

---
+++ Vue Introduction/task3/vue-clicker/src/components/Clicker.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="clicker">
+    <p>Очки: {{ count }}</p>
+    <button @click="increment">Тык</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+let count = ref(0)
+
+function increment() {
+  count.value += 1
+}
+</script>
+
+<style scoped>
+.clicker {
+  margin: 10px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  display: inline-block;
+}
+</style>
