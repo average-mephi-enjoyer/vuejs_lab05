043f6d8ee497fe4df20c1f9a0a8624249373aec0
Yashchenko Artem <artemyaschenko@inbox.ru>
2026-04-23 11:25:21 +0300

    task 1 prim

---
+++ Toolkit Postman/task1/script.js
@@ -0,0 +1,25 @@
+
+window.addEventListener('load', async () => {
+    let app = new PIXI.Application();
+    await app.init({
+        background: '#1099bb',
+        resizeTo: window,
+    });
+    document.body.appendChild(app.canvas);
+
+    let rectangle = new PIXI.Graphics();
+    rectangle.rect(0, 0, 100, 100);
+    rectangle.fill(0xff0000);
+    rectangle.pivot.set(50, 50);
+    rectangle.x = app.screen.width / 2;
+    rectangle.y = app.screen.height / 2;
+    app.stage.addChild(rectangle);
+
+    app.ticker.add(() => {
+        rectangle.rotation += 0.02;
+    });
+    window.addEventListener('resize', () => {
+        rectangle.x = app.screen.width / 2;
+        rectangle.y = app.screen.height / 2;
+    });
+});
