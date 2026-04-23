747e376dfcd2dbe93380d75b16edff4c70fe2d3f
Yashchenko Artem <artemyaschenko@inbox.ru>
2026-04-23 11:33:10 +0300

    task1 ready

--- Toolkit Postman/task1/script.js
+++ Toolkit Postman/task1/script.js
@@ -2,14 +2,14 @@
 window.addEventListener('load', async () => {
     let app = new PIXI.Application();
     await app.init({
-        background: '#1099bb',
+        background: '#d2d7d8',
         resizeTo: window,
     });
     document.body.appendChild(app.canvas);
 
     let rectangle = new PIXI.Graphics();
     rectangle.rect(0, 0, 100, 100);
-    rectangle.fill(0xff0000);
+    rectangle.fill(0x0000ff);
     rectangle.pivot.set(50, 50);
     rectangle.x = app.screen.width / 2;
     rectangle.y = app.screen.height / 2;
