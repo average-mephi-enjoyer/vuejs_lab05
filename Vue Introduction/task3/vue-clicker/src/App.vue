dc1dddba1fdf2762e367a27b598f4c3518a15578
Yashchenko Artem <artemyaschenko@inbox.ru>
2026-04-23 22:31:23 +0300

    task 9 done

--- Vue Introduction/task3/vue-clicker/src/App.vue
+++ Vue Introduction/task3/vue-clicker/src/App.vue
@@ -3,17 +3,19 @@
       <h1>100 Vue Clickers</h1>
       <div></div>
       <div class="clicker-grid">
-        <Clicker v-for="n in 90" :key="'def' + n" :amount="67" />
-        <Clicker :key="'custom1'" :amount="52"><strong>Тык</strong></Clicker>
-        <Clicker :key="'custom2'" :amount="53"><span style="font-size: 20px;">🚀</span></Clicker>
-        <Clicker v-for="n in 7" :key="'extra' + n" :amount="552"><em>Нажми</em></Clicker>
-        <Clicker @button-clicked="console.log('Клик!')" />
+        <Clicker v-for="n in 90" :key="'def' + n" :amount="52" @button-clicked="handleClick(n)" />
+        <Clicker :key="'custom1'" :amount="67" @button-clicked="handleClick(91)"><strong>Тык</strong></Clicker>
+        <Clicker :key="'custom2'" :amount="52" @button-clicked="handleClick(92)"><span style="font-size: 20px;">🚀</span></Clicker>
+        <Clicker v-for="k in 8" :key="'extra' + k + 92" :amount="552" @button-clicked="handleClick(k+92)"><em>Нажми</em> </Clicker>
       </div>
     </div>
 </template>
 
 <script setup>
 import Clicker from './components/Clicker.vue'
+function handleClick(n) {
+  alert(`Clicker ${n} clicked!`)
+}
 </script>
 
 <style>
